afd334dad27882e90811b932441e9715b4722fcf
kaz0oo <cole.ender@outlook.com>
2020-10-22 21:15:29 -0400

    Upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import tools
+
+print(tools.roll_dice(10))
+'''
+try:
+    #value = 10 / 0
+    number = int(input("Enter a number: "))
+    print(number)
+except ZeroDivisionError:
+    print("Divided by zero")
+except ValueError:
+    print("Invalid input")
+'''
---
+++ start.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+import webbrowser
+
+subprocess.call("atom")
+
+
+webbrowser.get("firefox").open("https://outlook.live.com/")
+
+
+os.system("libreoffice -o /home/marco/Documents/Excel/Simpatico.ods")
